43bbe4c7528ca21f0cd44ebb04a0d824f1940fe1
Sun Tan <sutan@redhat.com>
2021-03-18 18:26:17 +0000

    Check missing title element and throw Exception to clarify this instead of NPE. Part of #6.
    Signed-off-by: Sun Tan <sutan@redhat.com>

---
+++ src/main/java/org/parisjug/eventpublisher/eventpage/EventPageCheckException.java
@@ -0,0 +1,11 @@
+package org.parisjug.eventpublisher.eventpage;
+
+public class EventPageCheckException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public EventPageCheckException(String message) {
+        super(message);
+    }
+
+}
