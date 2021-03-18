6067dce3ac5bcef81f7169dcb009f71fd03b75c8
Sun Tan <sutan@redhat.com>
2021-03-18 18:27:54 +0000

    Improving message if no commands is specify. Fix #7.
    Signed-off-by: Sun Tan <sutan@redhat.com>

--- src/main/java/org/parisjug/eventpublisher/EventPublisher.java
+++ src/main/java/org/parisjug/eventpublisher/EventPublisher.java
@@ -5,6 +5,8 @@ import org.parisjug.eventpublisher.commands.GenerateGoogleCalendarCommand;
 
 import io.quarkus.picocli.runtime.annotations.TopCommand;
 import picocli.CommandLine;
+import picocli.CommandLine.Spec;
+import picocli.CommandLine.Model.CommandSpec;
 
 @TopCommand
 @CommandLine.Command(subcommands = { //
@@ -12,8 +14,11 @@ import picocli.CommandLine;
         CreateMailCampaignCommand.class //
 })
 public class EventPublisher implements Runnable {
+    @Spec
+    CommandSpec spec;
+
     @Override
     public void run() {
-        System.out.println("hello ");
+        throw new CommandLine.ParameterException(spec.commandLine(), "No command argument specified. Please provide a command argument.");
     }
 }
